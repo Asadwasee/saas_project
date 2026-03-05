8457c02cb225569e711db72f9fad5db78b51ab90
Asad Waseem <asadwaseem299@gmail.com>
2026-03-05 23:21:20 +0500

    Update backend logic

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -12,7 +12,7 @@ exports.createBlog = async (req, res) => {
             content,
             category,
             image,
-            author: req.user.id // Ye protect middleware se aayega
+            author: req.user.id 
         });
 
         res.status(201).json(blog);
@@ -29,6 +29,33 @@ exports.getBlogs = async (req, res) => {
         res.json(blogs);
     } catch (error) {
         res.status(500).json({ message: error.message });
+    }
+};
+
+// @desc    Update a blog
+// @route   PUT /api/blogs/:id
+exports.updateBlog = async (req, res) => {
+    try {
+        const { title, slug, content, category, image, description } = req.body;
+        
+        const blog = await Blog.findById(req.params.id);
+
+        if (!blog) {
+            return res.status(404).json({ message: "Blog not found" });
+        }
+
+        // Data update karein
+        blog.title = title || blog.title;
+        blog.slug = slug || blog.slug;
+        blog.content = content || blog.content;
+        blog.category = category || blog.category;
+        blog.image = image || blog.image;
+        blog.description = description || blog.description;
+
+        const updatedBlog = await blog.save();
+        res.json(updatedBlog);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
 };
 
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createBlog, getBlogs, deleteBlog } = require('../controllers/blogController');
+const { createBlog, getBlogs, deleteBlog, updateBlog } = require('../controllers/blogController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.get('/', getBlogs); // Public can see blogs
-router.post('/', protect, createBlog); // Only admin (logged in) can post
+router.get('/', getBlogs); 
+router.post('/', protect, createBlog); 
+router.put('/:id', protect, updateBlog); 
 router.delete('/:id', protect, deleteBlog);
 
 module.exports = router;
